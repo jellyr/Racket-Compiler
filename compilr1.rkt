d949e9368191051c6737025c4bc8138bbc5bd852
Srikanth Kanuri <srkanuri@iu.edu>
2016-01-16 21:00:41 -0500

    Almost complete print

--- compilr1.rkt
+++ compilr1.rkt
@@ -50,14 +50,14 @@
 (define (select-instructions-assign e)
   (match e
     [(? fixnum?) `(int ,e)]
-    [(? symbol?) #:when (not (eq? e 'program)) `(var ,e)]
+    [(? symbol?) #:when (not (eq? e 'program)) `(var ,e)] 
     [`(assign ,var (- ,e1)) `((movq ,(select-instructions-assign e1) (var ,var)) (negq (var ,var)))]
     [`(assign ,var (+ ,e1 ,e2))#:when (eq? var e1) `((addq ,(select-instructions-assign e2) (var ,var)))]
     [`(assign ,var (+ ,e1 ,e2))#:when (eq? var e2) `((addq ,(select-instructions-assign e1) (var ,var)))]
     [`(assign ,var (+ ,e1 ,e2)) `((movq ,(select-instructions-assign e1) (var ,var)) (addq ,(select-instructions-assign e2) (var ,var)))]
-    [`(return ,e1) `((addq ,(select-instructions-assign e1) (reg rax)))]
-    [else `(,e)]
-    ))
+    [`(return ,e1) `((movq ,(select-instructions-assign e1) (reg rax)))]
+    [`(assign ,var ,e1) `((movq ,(select-instructions-assign e1) (var ,var)))]
+    [else `(,e)]))
 
 (define (select-instructions e)
   (append-map select-instructions-assign e))
@@ -77,6 +77,39 @@
     [else e]))
 
 (define (assign-homes e)
-  (let ([env (assign-homes-env (cadr e) -1)])
-    (map (curryr assign-homes-var env) e)))
+  (let ([env (assign-homes-env (cadr e) -1)]
+        [prog (car e)])
+    `(,prog ,(length env) . ,(cddr (map (curryr assign-homes-var env) e)))))
 
+(define (patch-instr-helper e)
+  (match e
+    [`(movq (stack ,e1) (stack ,e2)) `((movq (stack ,e1) (reg rax)) (movq (reg rax) (stack ,e2)))]
+    [`(addq (stack ,e1) (stack ,e2)) `((movq (stack ,e1) (reg rax)) (addq (reg rax) (stack ,e2)))]
+    [else `(,e)]))
+
+(define (patch-instr e)
+    (append-map patch-instr-helper e))
+
+(define (print-helper e)
+  (match e
+    [`(stack ,e1) (format "~a(%rbp)" e1)]
+    [`(int ,e1) (format "$~a" e1)]
+    [`(reg ,e1) (format "%~a" e1)]
+    [`(movq ,e1 ,e2) (format "movq	~s, ~s\n\t" (print-helper e1) (print-helper e2))]
+    [`(negq ,e1) (format "negq	~s\n\t" (print-helper e1))]
+    [`(addq ,e1 ,e2) (format "addq	~s, ~s\n\t" (print-helper e1) (print-helper e2))]
+    ))
+
+(define (print e)
+  (string-append
+ (format "	.globl main
+main:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$~a, %rsp" (* 8 (cadr e)))
+ (string-join (map print-helper (cddr e)))
+ (format "	movq	%rax, %rdi
+	callq	print_int
+	addq	$~a, %rsp
+	popq	%rbp
+	retq" (* 8 (cadr e)))))
