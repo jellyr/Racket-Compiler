024fe0e9f6cbd7702b12436bebb851f7c784cb81
srkanuri <kanurisrikanth@yahoo.co.in>
2016-01-29 14:53:42 -0500

    Added Data2 and Data3

--- a2-test.rkt
+++ a2-test.rkt
@@ -17,6 +17,48 @@
             (movq (var z) (reg rax))
             (subq (var y) (reg rax))))
 
+(define data2 '(program
+  ((v w x y z)
+   ((v) (w v) (x w) (x w) (x w y) (x w y) (z w y) (z y) (rax y) ()))
+  (movq (int 1) (var v))
+  (movq (int 46) (var w))
+  (movq (var v) (var x))
+  (addq (int 7) (var x))
+  (movq (var x) (var y))
+  (addq (int 4) (var y))
+  (movq (var x) (var z))
+  (addq (var w) (var z))
+  (movq (var z) (reg rax))
+  (subq (var y) (reg rax))))
+
+(define data3 (list
+ 'program
+ (list
+  '(v w x y z)
+  (hash
+   'x
+   (set 'y 'w)
+   'w
+   (set 'v 'y 'z 'x)
+   'y
+   (set 'w 'z 'x 'rax)
+   'z
+   (set 'y 'w)
+   'rax
+   (set 'y)
+   'v
+   (set 'w)))
+ '(movq (int 1) (var v))
+ '(movq (int 46) (var w))
+ '(movq (var v) (var x))
+ '(addq (int 7) (var x))
+ '(movq (var x) (var y))
+ '(addq (int 4) (var y))
+ '(movq (var x) (var z))
+ '(addq (var w) (var z))
+ '(movq (var z) (reg rax))
+ '(subq (var y) (reg rax))))
+
 (define graph1
       (make-hash '()))
 (add-edge graph1 'a 'b)
