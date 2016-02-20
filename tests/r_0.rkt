ce84dae342b14f8786d0d2c0b51afe32da329494
srkanuri <kanurisrikanth@yahoo.co.in>
2016-02-20 16:44:08 -0500

    let fixed

--- tests/r_0.rkt
+++ tests/r_0.rkt
@@ -1,13 +1,13 @@
 (vector-ref
-  (let ([t1 (vector 1)]
-        [t2 (vector 1 2)]
-	[t3 (vector 1 2 3)]
-	[t4 (vector 1 2 3 4)]
-	[t5 (vector 1 2 3 4 5)]
-	[t6 (vector 1 2 3 4 5 6)]
-	[t7 (vector 1 2 3 4 5 6 7)]
-	[t8 (vector 1 2 3 4 5 6 7 8)]
-	[t9 (vector 1 2 3 4 5 6 7 8 9)]
-	[t0 (vector 1 2 3 4 5 6 7 8 9 10)])
-    t0)
+  (let ([t1 (vector 1)])
+  (let ([t2 (vector 1 2)])
+  (let ([t3 (vector 1 2 3)])
+  (let ([t4 (vector 1 2 3 4)])
+  (let ([t5 (vector 1 2 3 4 5)])
+  (let ([t6 (vector 1 2 3 4 5 6)])
+  (let ([t7 (vector 1 2 3 4 5 6 7)])
+  (let ([t8 (vector 1 2 3 4 5 6 7 8)])
+  (let ([t9 (vector 1 2 3 4 5 6 7 8 9)])
+  (let ([t0 (vector 1 2 3 4 5 6 7 8 9 10)])
+    t0))))))))))
   0)
