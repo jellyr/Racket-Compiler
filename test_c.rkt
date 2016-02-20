36757c72d99a15fa7c6a985ded0e0ec19bd22f1e
Yang Zhang <zhang505@indiana.edu>
2016-02-20 16:31:06 -0500

    fix test_c

--- test_c.rkt
+++ test_c.rkt
@@ -2,5 +2,5 @@
 (require "utilities.rkt")
 (require "compiler.rkt")
 
-(compiler-tests "compiler r" (curry typecheck-R2 '())
-                        r2-passes interp-scheme "r" (range 0 1))
+(compiler-tests "compiler r" typechecker
+                        r3-passes "r" (range 0 1))
