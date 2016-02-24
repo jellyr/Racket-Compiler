3b1e78b32f6d72502c8fe9a36e48dc67fbe7f447
srkanuri <kanurisrikanth@yahoo.co.in>
2016-02-24 17:02:31 -0500

    Changes done for patch instr 3

--- compiler.rkt
+++ compiler.rkt
@@ -643,11 +643,16 @@
 
 (define (patch-instr-helper e)
   (match e
-    [`(,op (offset (stack ,istack) ,index) ,e2)
-     `((movq (stack ,istack) (reg r11)) (,op (offset (reg r11) ,index) ,e2))] ;; e1 if offset stack
-    [`(,op ,e1 (offset (stack ,istack) ,index))
-     `((movq (stack ,istack) (reg r11)) (,op ,e1 (offset (reg r11) ,index)))] ;; e2 if offset stack
-    ;; negq[]
+    [`(,op (offset (stack ,istack) ,index) ,e2) (append `((movq (stack ,istack) (reg r11))
+                                                          (patch-instr-helper `(,op (offset (reg r11) ,index) ,e2))))]
+    ;; e1 if offset stack
+    [`(,op ,e1 (offset (stack ,istack) ,index)) (append `((movq (stack ,istack) (reg r11))
+                                                          (patch-instr-helper `(,op ,e1 (offset (reg r11) ,index)))))]
+    ;; e2 if offset stack
+    [`(movq ,e1 (offset ,e2 ,index)) #:when (stack? e1) `((movq ,e1 (reg rax))
+                                                          (movq (reg rax) (offset ,e2 ,index)))]
+    [`(movq (offset ,e1 ,index) ,e2) #:when (stack? e2) `((movq (offset ,e2 ,index) (reg rax))
+                                                          (movq (reg rax) ,e2))]
     [`(movq (global-value ,e1) ,e2) #:when (stack? e2) `((movq (global-value ,e1) (reg r11))
                                                          (movq (reg r11) ,e2))]
     [`(movq ,e1 ,e2) #:when (equal? e1 e2) '()]
