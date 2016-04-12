4bcc32786f126be767200a4a282b8f72a77b1a84
Yang <zhy0216@gmail.com>
2016-04-12 17:28:34 -0400

    works

--- pass/convert-to-closures.rkt
+++ pass/convert-to-closures.rkt
@@ -11,18 +11,19 @@
 
 (define (get-free-vars body env free)
   (match body
-    [`(has-type ,expr ,ht) #:when (symbol? expr) (if (assoc expr env)
-                                                     free
-                                                     (set-union free (set `(,expr . ,ht))))]
     [`(has-type (let ,vars ,b) ,ht) (set-union (foldr (lambda (e res)
-                                                        (set-union res
-                                                                   (get-free-vars e env free)))
-                                                      (set) (map last vars))
+                                                       (set-union res
+                                                                  (get-free-vars e env free)))
+                                                     (set) (map last vars))
                                                (get-free-vars b (append env (map car vars)) free))]
-    [`(has-type (lambda: ,vars : ,ret ,b) ,ht) (get-free-vars b (append env vars) free)]
     [`(has-type (vector-set! ,e1 ,e2 ,e3) ,ht) (set-union (get-free-vars e1 env free)
                                                           (get-free-vars e2 env free)
                                                           (get-free-vars e3 env free))]
+    [`(has-type ,expr ,ht) #:when (symbol? expr) (if (assoc expr env)
+                                                     free
+                                                     (set-union free (set `(,expr . ,ht))))]
+    
+    [`(has-type (lambda: ,vars : ,ret ,b) ,ht) (get-free-vars b (append env vars) free)]
     [`(has-type (,op ,e1 ,e2) ,ht) (set-union (get-free-vars e1 env free)
                                               (get-free-vars e2 env free))]
     [`(has-type (,op ,e1) ,ht) (get-free-vars e1 env free)]
@@ -36,10 +37,7 @@
     [(> idx (length fvs)) (clos-conv-helper body)]
     [else (let ([expr (fvs-let-builder body cvar clos-var-types fvs (add1 idx))])
             (match-define `(has-type ,b ,ht) expr)
-            `(has-type (let ([,(car (list-ref fvs (sub1 idx)))
-                              (has-type (vector-ref (has-type ,cvar (Vector _))
-                                                    (has-type ,idx Integer))
-                                        ,(list-ref clos-var-types (sub1 idx)))])
+            `(has-type (let ([,(car (list-ref fvs (sub1 idx))) (has-type (vector-ref ,cvar ,idx) ,(list-ref clos-var-types (sub1 idx)))])
                          ,expr) ,ht))]))
 
 (define (clos-conv-helper expr)
@@ -77,10 +75,9 @@
                                           [fune^ (clos-conv-helper e)])
                                       (match-define `(has-type ,expr1 ,ht1) e)
                                       (match-define `(has-type ,funexpr2 ,funht2) fune^)
-                                      ;; (set! funht2 (lookup funexpr2 fun-env (if (eqv? (car funht2) 'Vector)
-                                      ;;                                           funht2
-                                      ;;                                           `(Vector ,funht2))))
-                                      (match-define `(has-type ,fun-expr ,fun-type) fune^)
+                                      (set! funht2 (lookup funexpr2 fun-env (if (eqv? (car funht2) 'Vector)
+                                                                                funht2
+                                                                                `(Vector ,funht2))))
                                       ;(display "expr: ") (displayln expr)
                                       ;(display "e: ") (displayln e)
                                       ;(display "fune^: ") (displayln fune^)
@@ -115,13 +112,6 @@
     ;
     [`(has-type (vector . ,e) ,t) (let ([vec-vals (map clos-conv-helper e)])
                                     `(has-type (vector . ,vec-vals) (Vector . ,(map last vec-vals))))]
-    [`(has-type (vector-ref ,vect ,idx) ,ty)
-     (match-let ([`(has-type ,vect-name ,vect-type) (clos-conv-helper vect)]
-                 [`(has-type ,val ,val-type) (clos-conv-helper idx)])
-       (define v-type (lookup vect-name fun-env vect-type))
-       `(has-type (vector-ref
-                   (has-type ,vect-name ,v-type)
-                   (has-type ,val ,val-type)) ,(list-ref v-type (add1 val))))]
     [`(has-type (vector-set! ,vect (has-type ,index Integer) ,value) ,ht)
      (match-let ([`(has-type ,vect-name ,vect-type) (clos-conv-helper vect)]
                  [`(has-type ,value-expr ,value-type) (clos-conv-helper value)])
@@ -132,7 +122,10 @@
     [`(has-type (,op ,e1 ,e2) ,ht) ;;(displayln op) (displayln e2)
      `(has-type (,op ,(clos-conv-helper e1) ,(clos-conv-helper e2)) ,ht)]
     [`(has-type (,op ,e1) ,ht) `(has-type (,op ,(clos-conv-helper e1)) ,ht)]
-    [`(has-type ,e ,t) `(has-type ,e ,(lookup e fun-env t))]
+    [`(has-type ,e ,t)
+     ;(display "expr: ") (displayln expr)
+     ;(display "env: ") (displayln fun-env)
+     `(has-type ,e ,(lookup e fun-env t))]
     [else expr]))
 
 (define (convert-to-closures expr)
