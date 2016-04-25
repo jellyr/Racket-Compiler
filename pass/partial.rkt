6bebcda9fdda6342ba69fec09c751deae1c93c7b
Yang <zhy0216@gmail.com>
2016-04-25 13:50:46 -0400

    add partial

---
+++ pass/partial.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(require "../common.rkt")
+
+;;; in this file we need to eval the s-expression
+
+
+;;; define some simple example
+
+
+;;; let para estimate : the var is not used in let expression
+
+;;; let eval
+
+;; if statement eval: the statement is true or false
+
+;; + -, eval, some case read textbook
+
+;;
